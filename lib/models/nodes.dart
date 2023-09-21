0677d354a9e4b6f5d968c2e890c65b677e71e5ea
Alexandre WENG <alexandre.weng@epita.fr>
2023-09-21 15:05:25 -0400

    feat(nodes): added all node enums

--- lib/models/nodes.dart
+++ lib/models/nodes.dart
@@ -1,19 +1,38 @@
 enum Nodes {
   start,
+  mid,
+  intermediates,
+  penultimate,
   end,
   all,
-  allShooting; // TODO add the other node
+  allShooting,
+  transition,
+  multinode,
+  defaultNode,
+  ;
 
   String toPythonString() {
     switch (this) {
       case start:
         return 'Node.START';
+      case mid:
+        return 'Node.MID';
+      case intermediates:
+        return 'Node.INTERMEDIATES';
+      case penultimate:
+        return 'Node.PENULTIMATE';
       case end:
         return 'Node.END';
       case all:
         return 'Node.ALL';
       case allShooting:
         return 'Node.ALL_SHOOTING';
+      case transition:
+        return 'Node.TRANSITION';
+      case multinode:
+        return 'Node.MULTINODE';
+      case defaultNode:
+        return 'Node.DEFAULT';
     }
   }
 
@@ -22,12 +41,24 @@ enum Nodes {
     switch (this) {
       case start:
         return 'Starting node';
+      case mid:
+        return 'Mid node';
+      case intermediates:
+        return 'Intermediate nodes';
+      case penultimate:
+        return 'Penultimate node';
       case end:
         return 'Last node';
       case all:
-        return 'All nodes';
+        return 'All nodes ';
       case allShooting:
         return 'All shooting nodes';
+      case transition:
+        return 'Transition nodes';
+      case multinode:
+        return 'Multinode';
+      case defaultNode:
+        return 'Default node';
     }
   }
 }
