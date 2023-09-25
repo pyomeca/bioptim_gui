c99a864a96b497239a148ff178086a6dd1020304
Alexandre WENG <alexandre.weng@epita.fr>
2023-09-25 12:00:22 -0400

    feat: added boolean_switch.dart widget

---
+++ lib/widgets/boolean_switch.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class BooleanSwitch extends StatefulWidget {
+  final bool initialValue;
+  final ValueChanged<bool> customOnChanged;
+  final String leftTextOn;
+  final String leftTextOff;
+  final double width;
+
+  const BooleanSwitch({
+    Key? key,
+    required this.customOnChanged,
+    required this.initialValue,
+    required this.leftTextOn,
+    required this.leftTextOff,
+    required this.width, // Add width as a parameter
+  }) : super(key: key);
+
+  @override
+  BooleanSwitchState createState() => BooleanSwitchState();
+}
+
+class BooleanSwitchState extends State<BooleanSwitch> {
+  late bool lightValue;
+  late String leftText;
+
+  @override
+  void initState() {
+    super.initState();
+    lightValue = widget.initialValue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.width, // Set the width based on the widget's parameter
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(lightValue ? widget.leftTextOn : widget.leftTextOff),
+          Switch(
+            value: lightValue,
+            onChanged: (bool value) {
+              setState(() {
+                lightValue = value;
+              });
+              widget.customOnChanged(value);
+            },
+            thumbIcon: MaterialStateProperty.resolveWith<Icon?>(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.selected)) {
+                  return const Icon(Icons.check);
+                }
+                return const Icon(Icons.close);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
