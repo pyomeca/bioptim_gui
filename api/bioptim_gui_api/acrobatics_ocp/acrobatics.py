bbad7c16b2a7fa0b8213d3fbfa34eea7c1bb179d
Alexandre WENG <alexandre.weng@epita.fr>
2023-11-07 16:38:23 -0500

    refactor: moved read/update and add/remove fcn - removed from acrobatics.py and moved to acrobatics_utils, as acrobatics.py should only have endpoints for file coherence - removed half_twists from acrobatics_ocp/models.py as it is not used anymore (even if the model wasn't used)

--- api/bioptim_gui_api/acrobatics_ocp/acrobatics.py
+++ api/bioptim_gui_api/acrobatics_ocp/acrobatics.py
@@ -1,11 +1,9 @@
-import json
-
 from fastapi import APIRouter, HTTPException
 
 import bioptim_gui_api.acrobatics_ocp.acrobatics_code_generation as acrobatics_code_generation
-import bioptim_gui_api.acrobatics_ocp.acrobatics_config as config
 import bioptim_gui_api.acrobatics_ocp.acrobatics_phases as acrobatics_somersaults
 from bioptim_gui_api.acrobatics_ocp.acrobatics_responses import *
+from bioptim_gui_api.acrobatics_ocp.acrobatics_utils import *
 
 router = APIRouter(
     prefix="/acrobatics",
@@ -18,64 +16,6 @@ router.include_router(
     responses={404: {"description": "Not found"}},
 )
 router.include_router(acrobatics_code_generation.router)
-
-
-def add_somersault_info(n: int = 1) -> None:
-    # rounding is necessary to avoid buffer overflow in the frontend
-    if n < 1:
-        raise ValueError("n must be positive")
-
-    data = read_acrobatics_data()
-    phases_info = data["phases_info"]
-    before = len(phases_info)
-    n_somersaults = before + n
-    final_time = data["final_time"]
-    for i in range(0, before):
-        phases_info[i]["duration"] = round(final_time / n_somersaults, 2)
-
-    for i in range(before, before + n):
-        phases_info.append(config.DefaultAcrobaticsConfig.default_phases_info)
-        data["nb_half_twists"].append(0)
-
-        phases_info[i]["duration"] = round(final_time / n_somersaults, 2)
-
-    data["phases_info"] = phases_info
-    with open(config.DefaultAcrobaticsConfig.datafile, "w") as f:
-        json.dump(data, f)
-
-
-def remove_somersault_info(n: int = 0) -> None:
-    if n < 0:
-        raise ValueError("n must be positive")
-    data = read_acrobatics_data()
-    phases_info = data["phases_info"]
-    before = len(phases_info)
-    n_somersaults = before - n
-    final_time = data["final_time"]
-    for i in range(0, n_somersaults):
-        phases_info[i]["duration"] = round(final_time / n_somersaults, 2)
-
-    for _ in range(n):
-        phases_info.pop()
-        data["nb_half_twists"].pop()
-
-    data["phases_info"] = phases_info
-    with open(config.DefaultAcrobaticsConfig.datafile, "w") as f:
-        json.dump(data, f)
-
-
-def update_acrobatics_data(key: str, value) -> None:
-    with open(config.DefaultAcrobaticsConfig.datafile, "r") as f:
-        data = json.load(f)
-    data[key] = value
-    with open(config.DefaultAcrobaticsConfig.datafile, "w") as f:
-        json.dump(data, f)
-
-
-def read_acrobatics_data(key: str = None):
-    with open(config.DefaultAcrobaticsConfig.datafile, "r") as f:
-        data = json.load(f)
-    return data if key is None else data[key]
 
 
 @router.get("/", response_model=dict)
--- api/bioptim_gui_api/acrobatics_ocp/models.py
+++ api/bioptim_gui_api/acrobatics_ocp/models.py
@@ -3,9 +3,8 @@ from pydantic import BaseModel
 from bioptim_gui_api.penalty.models import Objective, Constraint
 
 
-class Somersault(BaseModel):
+class SomersaultPhase(BaseModel):
     nb_shooting_points: int
-    nb_half_twists: int
     duration: float
     objectives: list[Objective]
     constraints: list[Constraint]
