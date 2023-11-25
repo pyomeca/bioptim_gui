3e2d5fce7d5e206dd24433c87f6eb0de92b293be
Alexandre WENG <alexandre.weng@epita.fr>
2023-11-25 12:22:19 -0500

    wip: model_converter + tests - made generic model_converter to reduce code duplication - added beginning of tests for model converter

---
+++ api/bioptim_gui_api/model_converter/converter.py
@@ -0,0 +1,174 @@
+from abc import ABC, abstractmethod
+
+
+class BioModConverter:
+    """
+    Base class for bioMod model converters
+
+    Attributes
+    ----------
+    segment_rotation: dict
+        A dictionary containing the segment name as key and the rotation as value (e.g. "Pelvis" : "xyz")
+        that the model should contain
+    segment_translation: dict
+        A dictionary containing the segment name as key and the translation as value (e.g. "Pelvis" : "xyz")
+        that the model should contain
+    markers: list
+        A list of markers names that the model should contain
+    """
+
+    segment_rotation = dict
+    segment_translation = dict
+    markers = list()
+
+    @classmethod
+    def convert(cls, model_path: str) -> str:
+        """
+        'Convert' a bioptim model
+
+        - Checking that it contains all the required markers/segments according to the converter class attributes.
+        - Missing markers/segments will raise a ValueError.
+        - Not used markers/segments will be ignored but will stay in the model (removing them would remove some
+        segments that are required for the visual (e.g. the head is not needed but is better for the visual)).
+        - Rotations and translations will be updated if needed:
+            - If a segment rotation/translation is 'xzy' in the converter class and 'zx' in the model, it will be
+            updated to 'xzy'.
+            - If it contains more axes, the additional axes will be removed (e.g. 'xyz' -> 'xy'), or the line
+            completely removed if the converter class does not contain this segment.
+        - The order of the markers/segments is not changed.
+        - All rangesQ are removed from the model.
+
+        Parameters
+        ----------
+        model_path: str
+            The path to the bioptim model
+
+        Returns
+        -------
+        The updated bioMod model as a string
+
+        Raises
+        ------
+        ValueError
+            If the model does not contain all the required markers/segments
+        """
+        with open(model_path, "r") as f:
+            lines = f.readlines()
+
+        updated_lines = []
+        existing_segments = set()
+        existing_markers = set()
+
+        for line in lines:
+            if line.startswith("segment"):
+                segment_name = line.strip().split()[1]
+
+                if segment_name in cls.segment_rotation:
+                    existing_segments.add(segment_name)
+
+                    updated_lines.append(line)
+
+                    updated_lines.append(
+                        f"\trotations {cls.segment_rotation[segment_name]}\n"
+                    )
+                    continue
+
+                elif segment_name in cls.segment_translation:
+                    existing_segments.add(segment_name)
+
+                    updated_lines.append(line)
+
+                    updated_lines.append(
+                        f"\ttranslations {cls.segment_translation[segment_name]}\n"
+                    )
+                    continue
+
+            elif line.startswith("marker"):
+                marker_name = line.strip().split()[1]
+
+                if marker_name in cls.markers:
+                    existing_markers.add(marker_name)
+
+            updated_lines.append(line)
+
+        missing_segments = (
+            set(cls.segment_rotation) | set(cls.segment_translation)
+        ) - existing_segments
+        missing_markers = set(cls.markers) - existing_markers
+
+        if missing_segments or missing_markers:
+            raise ValueError(
+                f"Missing markers/segments: {', '.join(missing_segments | missing_markers)}"
+            )
+
+        return "".join(updated_lines)
+
+
+class StraightConverter(BioModConverter):
+    segment_rotation = {
+        "Pelvis": "xyz",
+        "Thorax": "xyz",
+        "RightUpperArm": "zy",
+        "LeftUpperArm": "zy",
+    }
+
+    segment_translation = {"Pelvis": "xyz"}
+
+
+class PikeConverter(StraightConverter):
+    segment_rotation = {
+        "Pelvis": "xyz",
+        "Thorax": "xyz",
+        "RightUpperArm": "zy",
+        "RightForearm": "zx",
+        "LeftUpperArm": "zy",
+        "LeftForearm": "zx",
+        "UpperLegs": "xy",
+    }
+
+    markers = [
+        "MiddleRightHand",
+        "TargetRightHand",
+        "MiddleLeftHand",
+        "TargetLeftHand",
+    ]
+
+
+class TuckConverter(PikeConverter):
+    segment_rotation = {
+        "Pelvis": "xyz",
+        "Thorax": "xyz",
+        "RightUpperArm": "zy",
+        "RightForearm": "zx",
+        "LeftUpperArm": "zy",
+        "LeftForearm": "zx",
+        "UpperLegs": "xy",
+        "LowerLegs": "x",
+    }
+
+
+class AcrobaticsWithVisual(ABC):
+    additional_segment_rotation = {"Head": "zx", "Eyes": "zx"}
+    additional_markers = ["eyes_vect_start", "eyes_vect_end", "fixation_front"]
+
+    @property
+    @abstractmethod
+    def segment_rotation(self):
+        return super().segment_rotation | self.additional_segment_rotation
+
+    @property
+    @abstractmethod
+    def markers(self):
+        return super().markers + self.additional_markers
+
+
+class StraightWithVisualConverter(StraightConverter, AcrobaticsWithVisual):
+    pass
+
+
+class PikeWithVisualConverter(PikeConverter, AcrobaticsWithVisual):
+    pass
+
+
+class TuckWithVisualConverter(TuckConverter, AcrobaticsWithVisual):
+    pass
--- api/tests/model_converter/test_model_converter.py
+++ api/tests/model_converter/test_model_converter.py
@@ -1,24 +1,78 @@
-from bioptim_gui_api.model_converter.pike_to_straight import pike_to_straight
-from bioptim_gui_api.model_converter.pike_to_tuck import pike_to_tuck
+import os
 
-PIKE_PATH = "test_biomods/pike.bioMod"
-TUCK_PATH = "test_biomods/tuck.bioMod"
-STRAIGHT_PATH = "test_biomods/straight.bioMod"
+import pytest
+
+from bioptim_gui_api.model_converter.converter import *
+
+BIOMODS_PATH = "../test_biomods"  # to change depending on from where you run the test
+
+WITHOUT_VISUAL_BASE_PATH = f"{BIOMODS_PATH}/without_visual/without_visual_base.bioMod"
+WITH_VISUAL_BASE_PATH = f"{BIOMODS_PATH}/with_visual/with_visual_base.bioMod"
 
 
-def test_pike_to_tuck():
-    with open(TUCK_PATH, "r") as f:
-        expected = f.read()
-
-    actual = pike_to_tuck(PIKE_PATH)
-
+@pytest.mark.parametrize(
+    ("position", "converter"),
+    [
+        ("straight", StraightConverter),
+        ("pike", PikeConverter),
+        ("tuck", TuckConverter),
+    ],
+)
+def test_without_visual(position, converter):
+    actual = converter.convert(WITHOUT_VISUAL_BASE_PATH)
+    expected = open(f"{BIOMODS_PATH}/without_visual/good/{position}.bioMod", "r").read()
     assert actual == expected
 
 
-def test_pike_to_straight():
-    with open(STRAIGHT_PATH, "r") as f:
-        expected = f.read()
+@pytest.mark.parametrize(
+    ("position", "converter"),
+    [
+        ("straight", StraightConverter),
+        ("pike", PikeConverter),
+        ("tuck", TuckConverter),
+    ],
+)
+def test_without_visual_bad(position, converter):
+    for reason in [
+        "missing_marker",
+        "missing_segment",
+        "missing_marker_and_segment",
+    ]:
+        filepath = f"{BIOMODS_PATH}/without_visual/{reason}/{position}.bioMod"
+        if os.path.exists(filepath):
+            with pytest.raises(ValueError):
+                converter.convert(filepath)
 
-    actual = pike_to_straight(PIKE_PATH)
 
+@pytest.mark.parametrize(
+    ("position", "converter"),
+    [
+        ("straight", StraightWithVisualConverter),
+        ("pike", PikeWithVisualConverter),
+        ("tuck", TuckWithVisualConverter),
+    ],
+)
+def test_with_visual(position, converter):
+    actual = converter.convert(WITHOUT_VISUAL_BASE_PATH)
+    expected = open(f"{BIOMODS_PATH}/with_visual/good/{position}.bioMod", "r").read()
     assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ("position", "converter"),
+    [
+        ("straight", StraightWithVisualConverter),
+        ("pike", PikeWithVisualConverter),
+        ("tuck", TuckWithVisualConverter),
+    ],
+)
+def test_with_visual_bad(position, converter):
+    for reason in [
+        "missing_marker",
+        "missing_segment",
+        "missing_marker_and_segment",
+    ]:
+        filepath = f"{BIOMODS_PATH}/with_visual/{reason}/{position}.bioMod"
+        if os.path.exists(filepath):
+            with pytest.raises(ValueError):
+                converter.convert(filepath)
