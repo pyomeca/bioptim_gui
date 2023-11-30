732ecded65b304a51eb616ef837056ba68aaccfc
Alexandre WENG <alexandre.weng@epita.fr>
2023-11-30 16:07:47 -0500

    test: qdot_init, tau init and bounds, baseline
    - added tests to compare baseline code, in case of future refactor

--- api/tests/variables/misc/test_q_dot_init.py
+++ api/tests/variables/misc/test_q_dot_init.py
@@ -1,21 +1,57 @@
+import pytest
+
 from bioptim_gui_api.variables.misc.pike_acrobatics_variables import PikeAcrobaticsVariables
 from bioptim_gui_api.variables.misc.straight_acrobatics_variables import StraightAcrobaticsVariables
 from bioptim_gui_api.variables.misc.tuck_acrobatics_variables import TuckAcrobaticsVariables
+from tests.variables.misc.pike_acrobatics_variables import PikeAcrobaticsVariables as BasePikeAcrobaticsVariables
+from tests.variables.misc.pike_with_visual_acrobatics_variables import (
+    PikeAcrobaticsWithVisualVariables as BasePikeAcrobaticsWithVisualVariables,
+    PikeAcrobaticsWithVisualVariables,
+)
+from tests.variables.misc.straight_acrobatics_variables import (
+    StraightAcrobaticsVariables as BaseStraightAcrobaticsVariables,
+)
+from tests.variables.misc.straight_with_visual_acrobatics_variables import (
+    StraightAcrobaticsWithVisualVariables as BaseStraightAcrobaticsWithVisualVariables,
+    StraightAcrobaticsWithVisualVariables,
+)
+from tests.variables.misc.tuck_acrobatics_variables import TuckAcrobaticsVariables as BaseTuckAcrobaticsVariables
+from tests.variables.misc.tuck_with_visual_acrobatics_variables import (
+    TuckAcrobaticsWithVisualVariables as BaseTuckAcrobaticsWithVisualVariables,
+    TuckAcrobaticsWithVisualVariables,
+)
 
 
-def test_qdot_init():
+def test_qdot_init_straight():
     expected = [0.0] * 10
     actual = StraightAcrobaticsVariables.get_qdot_init()
     assert actual == expected
 
 
-def test_qdot_init():
+def test_qdot_init_pike():
     expected = [0.0] * 17
     actual = TuckAcrobaticsVariables.get_qdot_init()
     assert actual == expected
 
 
-def test_qdot_init():
+def test_qdot_init_tuck():
     expected = [0.0] * 16
     actual = PikeAcrobaticsVariables.get_qdot_init()
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    ("variable_compute", "baseline"),
+    [
+        (StraightAcrobaticsVariables, BaseStraightAcrobaticsVariables),
+        (PikeAcrobaticsVariables, BasePikeAcrobaticsVariables),
+        (TuckAcrobaticsVariables, BaseTuckAcrobaticsVariables),
+        (StraightAcrobaticsWithVisualVariables, BaseStraightAcrobaticsWithVisualVariables),
+        (PikeAcrobaticsWithVisualVariables, BasePikeAcrobaticsWithVisualVariables),
+        (TuckAcrobaticsWithVisualVariables, BaseTuckAcrobaticsWithVisualVariables),
+    ],
+)
+def test_qdot_init_same_as_baseline(variable_compute, baseline):
+    expected = baseline.get_qdot_init()
+    actual = variable_compute.get_qdot_init()
     assert actual == expected
--- api/tests/variables/misc/test_tau_bounds.py
+++ api/tests/variables/misc/test_tau_bounds.py
@@ -1,9 +1,28 @@
+import pytest
+
 from bioptim_gui_api.variables.misc.pike_acrobatics_variables import PikeAcrobaticsVariables
 from bioptim_gui_api.variables.misc.straight_acrobatics_variables import StraightAcrobaticsVariables
 from bioptim_gui_api.variables.misc.tuck_acrobatics_variables import TuckAcrobaticsVariables
+from tests.variables.misc.pike_acrobatics_variables import PikeAcrobaticsVariables as BasePikeAcrobaticsVariables
+from tests.variables.misc.pike_with_visual_acrobatics_variables import (
+    PikeAcrobaticsWithVisualVariables as BasePikeAcrobaticsWithVisualVariables,
+    PikeAcrobaticsWithVisualVariables,
+)
+from tests.variables.misc.straight_acrobatics_variables import (
+    StraightAcrobaticsVariables as BaseStraightAcrobaticsVariables,
+)
+from tests.variables.misc.straight_with_visual_acrobatics_variables import (
+    StraightAcrobaticsWithVisualVariables as BaseStraightAcrobaticsWithVisualVariables,
+    StraightAcrobaticsWithVisualVariables,
+)
+from tests.variables.misc.tuck_acrobatics_variables import TuckAcrobaticsVariables as BaseTuckAcrobaticsVariables
+from tests.variables.misc.tuck_with_visual_acrobatics_variables import (
+    TuckAcrobaticsWithVisualVariables as BaseTuckAcrobaticsWithVisualVariables,
+    TuckAcrobaticsWithVisualVariables,
+)
 
 
-def test_tau_bounds():
+def test_tau_bounds_straight():
     expected_min = [-500.0, -500.0, -500.0, -500.0]
     expected_max = [500.0, 500.0, 500.0, 500.0]
     actual = StraightAcrobaticsVariables.get_tau_bounds()
@@ -12,7 +31,7 @@ def test_tau_bounds():
     assert actual_max == expected_max
 
 
-def test_tau_bounds():
+def test_tau_bounds_pike():
     expected_min = [-500.0] * 10
     expected_max = [500.0] * 10
     actual = PikeAcrobaticsVariables.get_tau_bounds()
@@ -21,10 +40,27 @@ def test_tau_bounds():
     assert actual_max == expected_max
 
 
-def test_tau_bounds():
+def test_tau_bounds_tuck():
     expected_min = [-500.0] * 11
     expected_max = [500.0] * 11
     actual = TuckAcrobaticsVariables.get_tau_bounds()
     actual_min, actual_max = actual["min"], actual["max"]
     assert actual_min == expected_min
     assert actual_max == expected_max
+
+
+@pytest.mark.parametrize(
+    ("variable_compute", "baseline"),
+    [
+        (StraightAcrobaticsVariables, BaseStraightAcrobaticsVariables),
+        (PikeAcrobaticsVariables, BasePikeAcrobaticsVariables),
+        (TuckAcrobaticsVariables, BaseTuckAcrobaticsVariables),
+        (StraightAcrobaticsWithVisualVariables, BaseStraightAcrobaticsWithVisualVariables),
+        (PikeAcrobaticsWithVisualVariables, BasePikeAcrobaticsWithVisualVariables),
+        (TuckAcrobaticsWithVisualVariables, BaseTuckAcrobaticsWithVisualVariables),
+    ],
+)
+def test_tau_bounds_same_as_baseline(variable_compute, baseline):
+    expected = baseline.get_tau_bounds()
+    actual = variable_compute.get_tau_bounds()
+    assert actual == expected
