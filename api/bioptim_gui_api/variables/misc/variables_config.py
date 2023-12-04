60e0a1116b867e650a39f86924572e9a6a9defe6
Alexandre WENG <alexandre.weng@epita.fr>
2023-12-04 16:42:00 -0500

    refactor: duplication state/control variables
    - less duplication for default state/control variables init values
    - added a function to do that job + added test for that functio

--- api/bioptim_gui_api/variables/misc/variables_config.py
+++ api/bioptim_gui_api/variables/misc/variables_config.py
@@ -1,69 +1,43 @@
+import copy
+
+from bioptim_gui_api.variables.misc.variables_utils import variables_zeros
+
+
+def default_bounds_initial_guess(
+    name: str,
+    dimension: int = 1,
+    bounds_interpolation_type: str = "CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT",
+    initial_guess_interpolation_type: str = "CONSTANT",
+):
+    bounds = variables_zeros(dimension, bounds_interpolation_type)
+    initial_guess = variables_zeros(dimension, initial_guess_interpolation_type)
+
+    return {
+        "name": f"{name}",
+        "dimension": 1,
+        "bounds_interpolation_type": f"{bounds_interpolation_type}",
+        "bounds": {"min_bounds": copy.deepcopy(bounds), "max_bounds": copy.deepcopy(bounds)},
+        "initial_guess_interpolation_type": f"{initial_guess_interpolation_type}",
+        "initial_guess": initial_guess,
+    }
+
+
 class DefaultVariablesConfig:
     default_dummy_variables = {
         "state_variables": [
-            {
-                "name": "coucou",
-                "dimension": 1,
-                "bounds_interpolation_type": "CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT",
-                "bounds": {
-                    "min_bounds": [[0.0, 0.0, 0.0]],
-                    "max_bounds": [[0.0, 0.0, 0.0]],
-                },
-                "initial_guess_interpolation_type": "CONSTANT",
-                "initial_guess": [[0.0]],
-            },
+            default_bounds_initial_guess("coucou"),
         ],
         "control_variables": [
-            {
-                "name": "tata",
-                "dimension": 1,
-                "bounds_interpolation_type": "CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT",
-                "bounds": {
-                    "min_bounds": [[0.0, 0.0, 0.0]],
-                    "max_bounds": [[0.0, 0.0, 0.0]],
-                },
-                "initial_guess_interpolation_type": "CONSTANT",
-                "initial_guess": [[0.0]],
-            },
+            default_bounds_initial_guess("tata"),
         ],
     }
 
     default_torque_driven_variables = {
         "state_variables": [
-            {
-                "name": "q",
-                "dimension": 1,
-                "bounds_interpolation_type": "CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT",
-                "bounds": {
-                    "min_bounds": [[0.0, 0.0, 0.0]],
-                    "max_bounds": [[0.0, 0.0, 0.0]],
-                },
-                "initial_guess_interpolation_type": "CONSTANT",
-                "initial_guess": [[0.0]],
-            },
-            {
-                "name": "qdot",
-                "dimension": 1,
-                "bounds_interpolation_type": "CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT",
-                "bounds": {
-                    "min_bounds": [[0.0, 0.0, 0.0]],
-                    "max_bounds": [[0.0, 0.0, 0.0]],
-                },
-                "initial_guess_interpolation_type": "CONSTANT",
-                "initial_guess": [[0.0]],
-            },
+            default_bounds_initial_guess("q"),
+            default_bounds_initial_guess("qdot"),
         ],
         "control_variables": [
-            {
-                "name": "tau",
-                "dimension": 1,
-                "bounds_interpolation_type": "CONSTANT_WITH_FIRST_AND_LAST_DIFFERENT",
-                "bounds": {
-                    "min_bounds": [[0.0, 0.0, 0.0]],
-                    "max_bounds": [[0.0, 0.0, 0.0]],
-                },
-                "initial_guess_interpolation_type": "CONSTANT",
-                "initial_guess": [[0.0]],
-            },
+            default_bounds_initial_guess("tau"),
         ],
     }
