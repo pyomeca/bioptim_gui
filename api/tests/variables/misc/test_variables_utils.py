f44d60c1ecaf34b9cffd2e8e5fae37915b6570dc
Alexandre WENG <alexandre.weng@epita.fr>
2023-12-05 10:30:40 -0500

    test: added tests for fig 14.5 angles function

--- api/tests/variables/misc/test_variables_utils.py
+++ api/tests/variables/misc/test_variables_utils.py
@@ -1,6 +1,7 @@
+import numpy as np
 import pytest
 
-from bioptim_gui_api.variables.misc.variables_utils import variables_zeros
+from bioptim_gui_api.variables.misc.variables_utils import variables_zeros, maximum_fig_arms_angle
 
 
 @pytest.mark.parametrize(
@@ -41,3 +42,46 @@ def test_variables_zeros(dimension, interpolation_type, expected):
 def test_variables_zeros_error():
     with pytest.raises(ValueError):
         variables_zeros(1, "NOT_IMPLEMENTED")
+
+
+@pytest.mark.parametrize(
+    "half_twists",
+    [
+        [0],
+        [1],
+        [2],
+        [0, 0],
+        [0, 1],
+        [1, 0],
+        [1, 1],
+        [2, 0],
+        [2, 1],
+        [3, 0],
+        [3, 1],
+        [4, 0],
+        [4, 1],
+    ],
+)
+def test_fig_max_arms_angle_45(half_twists):
+    maximum_fig_arms_angle(half_twists) == np.deg2rad(45)
+
+
+@pytest.mark.parametrize(
+    "half_twists",
+    [
+        [3],
+        [4],
+        [0, 2],
+        [0, 3],
+        [1, 2],
+        [1, 3],
+        [2, 2],
+        [2, 3],
+        [3, 2],
+        [3, 3],
+        [4, 2],
+        [4, 13],
+    ],
+)
+def test_fig_max_arms_angle_90(half_twists):
+    maximum_fig_arms_angle(half_twists) == np.deg2rad(90)
