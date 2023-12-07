ed25d9497aa0da45732f6b3ddfc1bac5bbf11158
Alexandre WENG <alexandre.weng@epita.fr>
2023-12-07 16:37:24 -0500

    refactor: moving constant inside function
    - the constants are not used outside the function

--- api/bioptim_gui_api/model_converter/utils.py
+++ api/bioptim_gui_api/model_converter/utils.py
@@ -1,33 +1,32 @@
 from bioptim_gui_api.model_converter.converter import *
 
 
-with_visual_segment_rotations = {"Head": "zx", "Eyes": "zx"}
-with_visual_markers = (
-    ["eyes_vect_start", "eyes_vect_end", "fixation_front", "fixation_center"]
-    + [f"Trampo_corner_{n}" for n in range(1, 5)]
-    + [f"cone_approx_{i}_{j}" for i in range(11) for j in range(10)]
-)
-
-non_collision_markers = [
-    "HeadTop",
-    "RightShoulder",
-    "RightElbow",
-    "RightKnuckle",
-    "LeftShoulder",
-    "LeftElbow",
-    "LeftKnuckle",
-    "PelvisBase",
-    "Knee",
-    "Ankle",
-]
-
-
 def get_converter(position: str = "straight", with_visual_criteria: bool = False, collision_constraint: bool = False):
     base_converters = {
         "straight": StraightConverter,
         "tuck": TuckConverter,
         "pike": PikeConverter,
     }
+
+    with_visual_segment_rotations = {"Head": "zx", "Eyes": "zx"}
+    with_visual_markers = (
+        ["eyes_vect_start", "eyes_vect_end", "fixation_front", "fixation_center"]
+        + [f"Trampo_corner_{n}" for n in range(1, 5)]
+        + [f"cone_approx_{i}_{j}" for i in range(11) for j in range(10)]
+    )
+
+    non_collision_markers = [
+        "HeadTop",
+        "RightShoulder",
+        "RightElbow",
+        "RightKnuckle",
+        "LeftShoulder",
+        "LeftElbow",
+        "LeftKnuckle",
+        "PelvisBase",
+        "Knee",
+        "Ankle",
+    ]
 
     base = base_converters[position]
 
